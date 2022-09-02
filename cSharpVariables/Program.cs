b85863a5feb93eb442d586fb2f8720219d58f40d
Ayeika-tech <68619379+Ayeika-tech@users.noreply.github.com>
2022-09-01 22:22:30 -0500

    Add project files.

---
+++ cSharpVariables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//I see no difference in Variables compared to Java.
+
+namespace cSharpVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 35;
+            char grade = 'A';
+            bool isFemale = false;
+
+
+            Console.WriteLine("My name is "+ name +"\nMy age is "+age);
+            Console.WriteLine("My test grade is "+ grade);
+            Console.WriteLine("Am I a Female? "+ isFemale);
+            Console.ReadLine();
+        }
+    }
+}
